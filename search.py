42b588cb29d4ddf442be5fc8ca147a59dcc7afc4
Abram Schonfeldt <schabr004@myuct.ac.za>
2020-10-20 17:57:46 +0200

    seach code implements A*, UCS, BFS and DFS

--- search.py
+++ search.py
@@ -114,31 +114,17 @@ def GraphSearch(problem, fringe):
 
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    """
+    """Search the deepest nodes in the search tree first."""
     return GraphSearch(problem, util.Stack())
     
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return GraphSearch(problem, util.Queue())
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return GraphSearch(problem, util.PriorityQueueWithFunction(lambda state: state[2])) #state[2] is the cumulative cost
 
 def nullHeuristic(state, problem=None):
     """
@@ -149,8 +135,7 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return GraphSearch(problem, util.PriorityQueueWithFunction( lambda state: state[2] + heuristic(state[0],problem) ) )
 
 
 # Abbreviations
